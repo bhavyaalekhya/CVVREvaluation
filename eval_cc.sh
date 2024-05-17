04c66f9144fdae432875a4d9ec301f537f86cdab
Bhavya Alekhya <bhavyaalekhya98@gmail.com>
2024-05-17 14:01:55 -0500

    Modification of files

---
+++ eval_cc.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DIR="ckpt/timechat"
+MODEL_DIR=${DIR}/timechat_7b.pth
+
+TASK='captaincook'
+ANNO_DIR='./captain_cook_dvc.json'
+VIDEO_DIR='/data/rohith/captain_cook/videos/gopro/resolution_360p/'
+DATASET='captaincook'
+SPLIT='test'
+PROMPT_FILE="./captain_cook_description_zeroshot.txt"
+GT_FILE="${ANNO_DIR}/${SPLIT}.caption_coco_format.json"
+#ASR_DIR='data/YouCook2-BB/YouCook2_asr_denseCap/whisper_outputs_with_time/small.en.cleaned/'
+
+NUM_FRAME=96
+OUTPUT_DIR=${DIR}/${TASK}
+
+python evaluate.py --anno_path ${ANNO_DIR} --video_path ${VIDEO_DIR} \
+--task ${TASK} --dataset ${DATASET} --output_dir ${OUTPUT_DIR} --split ${SPLIT} --num_frames ${NUM_FRAME} --batch_size 16 \
+--prompt_file ${PROMPT_FILE} --timechat_model_path ${MODEL_DIR} \
+#--asr --asr_path ${ASR_DIR}
+#--debug
+
+cd metrics/${TASK}
+python eval_${TASK}.py --pred_file "${OUTPUT_DIR}/fmt_${DATASET}_${SPLIT}_f${NUM_FRAME}_result.json" --gt_file ${GT_FILE} | tee "${OUTPUT_DIR}/fmt_${DATASET}_${SPLIT}_f${NUM_FRAME}_result.txt"
+cd ../..
