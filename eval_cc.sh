2d6a6485ca8f47628e15ef633b95144cbc56fbb5
Bhavya Alekhya <bhavyaalekhya98@gmail.com>
2024-05-17 14:03:18 -0500

    Update eval_cc.sh

--- eval_cc.sh
+++ eval_cc.sh
@@ -5,7 +5,7 @@ MODEL_DIR=${DIR}/timechat_7b.pth
 
 TASK='captaincook'
 ANNO_DIR='./captain_cook_dvc.json'
-VIDEO_DIR='/data/rohith/captain_cook/videos/gopro/resolution_360p/'
+VIDEO_DIR='/data/rohith/captain_cook/videos/gopro/resolution_360p/8_16_360p.mp4'
 DATASET='captaincook'
 SPLIT='test'
 PROMPT_FILE="./captain_cook_description_zeroshot.txt"
